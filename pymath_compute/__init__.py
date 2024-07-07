8e936f484b8fe65126c98a88c2b2484c4b1b8496
ricardoleal20 <rick.leal420@gmail.com>
2024-07-07 13:52:40 -0600

    ✏️ Types: Readjust the structure of the types model

--- pymath_compute/__init__.py
+++ pymath_compute/__init__.py
@@ -6,3 +6,10 @@ To better documentation, please refer to the Github Page.
     > https://github.com/ricardoleal20/pymath_compute
 """
 from pymath_compute.model import Variable, MathExpression, MathFunction
+
+# Include the all module
+__all__ = [
+    "Variable",
+    "MathExpression",
+    "MathFunction"
+]
--- pymath_compute/model/types.py
+++ pymath_compute/model/types.py
@@ -1,20 +1,25 @@
 """
 Get types to use in common around the model definition
 """
-from typing import TypeVar, Union, Dict, TYPE_CHECKING
+from typing import TypeVar, Dict, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from pymath_compute.model.variable import Variable
     from pymath_compute.model.expression import MathExpression
 
+# ===================================== #
+#               TypeVar                 #
+# ===================================== #
 Operators = TypeVar(
     "Operators",
     # Math the operators
     'Variable',  'MathExpression',  int, float
 )
 Term = TypeVar("Term", str, "Variable")
-
-MathematicalTerms = Dict[Term, float]
-
 # Define the bound
 Bound = TypeVar("Bound", int, float)
+
+# ===================================== #
+#                Terms                  #
+# ===================================== #
+MathematicalTerms = Dict[Term, float]
