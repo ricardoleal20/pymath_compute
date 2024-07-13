3553689e8655b49a22ee5027d76a0e99077273b6
ricardoleal20 <rick.leal420@gmail.com>
2024-07-12 20:20:25 -0600

    🚧 WIP: Add a better `build-method` in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,25 @@ use pyo3::prelude::*;
 /// The modules now includes in this engine are:
 ///     - methods: Include different set of methods
 #[pymodule]
-fn pymath_compute_engine(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(gradient_descent, m)?)?;
+fn engine(py: Python, m: &PyModule) -> PyResult<()> {
+    // Call all the modules and append those modules to the main module //
+    // MODULE METHODS.
+    let optimization = build_optimization_module(py)?;
+    // ================================================================= //
+    // Add all the PyModules to the main m module                        //
+    // ================================================================= //
+    m.add_submodule(optimization)?;
+    // Return the result of the module at the very end                   //
     Ok(())
+}
+
+/// Include several optimization methods to handle the optimization
+/// of different functions
+fn build_optimization_module(py: Python) -> Result<&PyModule, PyErr> {
+    // Let's add a new submodule for the methods
+    let methods_module = PyModule::new(py, "optimization_methods")?;
+    // Add the methods inside here
+    methods_module.add_function(wrap_pyfunction!(gradient_descent, methods_module)?)?;
+    // Return the methods module
+    Ok(methods_module)
 }
