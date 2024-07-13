6a0649157c4b46e2251e1c220da0e1a5fff5ecde
ricardoleal20 <rick.leal420@gmail.com>
2024-07-13 15:34:00 -0600

    🚚 Movement: Move files from one place to another

--- pymath_compute/methods/__init__.py
+++ pymath_compute/methods/__init__.py
@@ -1,24 +1,8 @@
 """
 Methods to use to solve this computation methods.
-
-In here, you can an enum of the methods that you can use
 """
-from enum import Enum
-# Make also local imports
-from pymath_compute.methods.training import gradient_descent
+from pymath_compute.methods.opt_methods import OptMethods
 
-
-class Methods(Enum):
-    """Available methods to use in the Solver
-    
-    Available methods are:
-        - Interpolation:
-            ...
-        - Regularity:
-            ...
-        - Search:
-            ...
-        - Training:
-            * GRADIENT_DESCENT
-    """
-    GRADIENT_DESCENT = gradient_descent
+__all__ = [
+    "OptMethods"
+]
