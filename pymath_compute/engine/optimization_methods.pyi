e346fadfd35d0bd588909193e74ff78bcd2ea624
ricardoleal20 <rick.leal420@gmail.com>
2024-07-16 22:14:46 -0600

    📝 Docs: Add documentation for the gradient method

--- pymath_compute/engine/optimization_methods.pyi
+++ pymath_compute/engine/optimization_methods.pyi
@@ -23,4 +23,20 @@ def gradient_descent(
     iterations: int,
     tol: float,
 ) -> STATUS:
-    """..."""
+    """Gradient Descent implementation
+    
+    This is a normal gradient descent implementation for a Python code.
+    The values of the variables are updated in each iteration of the
+    method, only if the old cost is better than the new cost.
+    
+    Args:
+        - variables (list[Variable]): Variables given by the PyMath Module
+        - cost_method (Callable): Method to calculate the cost.
+        - var_step (float): Finite step to calculate the gradient
+        - learning_rate (float): The learning rate for the variables
+        - iterations (int): How many iterations are you going to run as max
+        - tol (float): The tolerance to know if you got an optimal
+    
+    Returns:
+        - The status of the method
+    """
